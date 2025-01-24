c671e3431569450426d25171f4b3ee0948c5cbfd
PedroMiguelFontes <58701236+PedroMiguelFontes@users.noreply.github.com>
2025-01-24 16:10:24 +0000

    check account

--- MusicApp/src/views/account.vue
+++ MusicApp/src/views/account.vue
@@ -1,16 +1,55 @@
 <template>
     <div>
-        <img src="" alt="account image">
-        <button @clikc="''">Change info</button>
+      <h1>Account Page</h1>
+      <img :src="userImage" alt="account image">
+      <button @click="changeInfo">Change info</button>
+      <div v-if="showForm">
+        <h2>Change Account Info</h2>
+        <form @submit.prevent="updateInfo">
+          <label for="username">Username:</label>
+          <input type="text" id="username" v-model="newUsername" required><br>
+          <label for="password">Password:</label>
+          <input type="password" id="password" v-model="newPassword" required><br>
+          <button type="submit">Update</button>
+          <button @click="cancelUpdate">Cancel</button>
+        </form>
+      </div>
     </div>
-</template>
-
-<script>
-    export default {
-        
+  </template>
+  
+  <script>
+  import { userStore } from '@/stores/users';
+  
+  export default {
+    data() {
+      return {
+        showForm: false,
+        newUsername: '',
+        newPassword: '',
+        userImage: '@/assets/image.png'
+      }
+    },
+    computed: {
+      store() {
+        return userStore();
+      }
+    },
+    methods: {
+      changeInfo() {
+        this.showForm = true;
+      },
+      updateInfo() {
+        this.store.updateUserInfo(this.newUsername, this.newPassword);
+        console.log(`Updating info to Username: ${this.newUsername}, Password: ${this.newPassword}`);
+        this.showForm = false;
+      },
+      cancelUpdate() {
+        this.showForm = false;
+      }
     }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+  /* Add your styles here */
+  </style>
